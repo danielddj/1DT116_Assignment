4c522c062af8191a42a0077a4b2f85546eaf6659
Philxy <philxyphilxy@gmail.com>
2025-03-05 14:10:37 +0100

    cuda heatmap

--- libpedsim/heatmap_cuda.cu
+++ libpedsim/heatmap_cuda.cu
@@ -315,7 +315,7 @@ void Ped::Model::updateHeatmapCUDA()
         */
 }
 
-void Ped::Mode::synchronizeCUDAHeatmapCalc()
+void Ped::Model::synchronizeCUDAHeatmapCalc()
 {
     cudaDeviceSynchronize();
     cudaMemcpy(blurred_heatmap[0], dev_blurred_heatmap,
