3ac0c5399e63494c72f20d330e913c006cfa6bf8
Philxy <philxyphilxy@gmail.com>
2025-03-10 00:58:11 +0100

    timings

--- libpedsim/ped_model.cpp
+++ libpedsim/ped_model.cpp
@@ -377,6 +377,9 @@ Ped::Model::~Model()
   std::for_each(destinations.begin(), destinations.end(),
                 [](Ped::Twaypoint *destination)
                 { delete destination; });
+
+
+  printHeatmapTimingSummary();
 }
 
 void Ped::Model::init_region(size_t start_regions, size_t width, size_t height,
