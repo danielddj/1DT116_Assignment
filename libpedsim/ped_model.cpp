aebda11ffe6c3f7ef0f7ad7df4aa51c7764774c8
Philxy <philxyphilxy@gmail.com>
2025-03-10 17:55:40 +0100

    global mem

--- libpedsim/ped_model.cpp
+++ libpedsim/ped_model.cpp
@@ -157,7 +157,7 @@ void Ped::Model::heat_cuda_tick()
 
   // Firstly, launch the CUDA kernels to compute the heat map
   auto start = std::chrono::high_resolution_clock::now();
-  //updateHeatmapCUDA();
+  updateHeatmapCUDA();
   auto end = std::chrono::high_resolution_clock::now();
   std::chrono::duration<double, std::milli> elapsed = end - start;
   launchTime += elapsed.count();
@@ -171,7 +171,7 @@ void Ped::Model::heat_cuda_tick()
 
   // Wait for GPU to finish, as we need the new agents positions to calculate the heat map in the next tick
   auto startSync = std::chrono::high_resolution_clock::now();
-  //synchronizeCUDAHeatmapCalc();
+  synchronizeCUDAHeatmapCalc();
   auto endSync = std::chrono::high_resolution_clock::now();
   elapsed = endSync - startSync;
   totalSyncTime += elapsed.count();
